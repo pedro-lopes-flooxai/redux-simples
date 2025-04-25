ce08b374ae8b81b95895016f314fb370e6fa2a1d
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-25 09:19:09 -0300

    add: getColor

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -6,7 +6,16 @@ function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
-      <Card></Card>
+      <div className='linha'>
+        <Card tittle='Card 1' red>
+x
+        </Card>
+      </div>
+      <div className='linha'>
+        <Card tittle='Card 2'>
+y
+        </Card>
+      </div>
     </div>
   );
 }
