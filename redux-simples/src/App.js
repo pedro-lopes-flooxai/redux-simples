d3cef36eb16e245c58740e5e707bc0b47e418899
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-25 08:52:43 -0300

    add: card

---
+++ redux-simples/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Exercício React-Redux (Simples)</h1>
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
