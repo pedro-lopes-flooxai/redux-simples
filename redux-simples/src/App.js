e9d0b71d6a93dec5f7702a27cdfa856af758d775
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-25 11:09:04 -0300

    add: min and max

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,20 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import Card from './components/Card';
+
+import Intervalo from './components/Intervalo';
+import Media from './components/Media';
+import Soma from './components/Soma';
+import Sorteio  from './components/Sorteio';
 
 function App() {
+
+const [min, setMin] = useState(10)
+const [max, setMax] = useState(20)
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className='linha'>
-        <Card tittle='Card 1' red>
-x
-        </Card>
+        <Intervalo min={min} max={max}
+         onMinChanged={setMin} onMaxChanged={setMax}></Intervalo>
       </div>
       <div className='linha'>
-        <Card tittle='Card 2'>
-y
-        </Card>
+        <Media min={min} max={max}></Media>
+        <Soma min={min} max={max}></Soma>
+        <Sorteio min={min} max={max}></Sorteio>
       </div>
     </div>
   );
